10643717a0db76d05b0f83a289f932c9ac0feae9
ramakusuma10 <ramakusuma098@gmail.com>
2024-06-24 17:00:44 +0700

    feat : implement email forgot password

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, VerificationType } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 import { UserType } from '../types/types'
 import { JWT_SECRET } from '../configs/config'
 import jwt from 'jsonwebtoken'
